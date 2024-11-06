77844155dc9bee9e2969098d97abcec00849d962
alessandroguddo02 <guddoalessandro6@gmail.com>
2024-11-06 16:03:06 +0100

    first exercise completed

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,10 +4,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function createArray (newArray){                    //creo funzione
+    let myArray = [];                               //creo variabile di accumulo
+    for(let i = 0; i < newArray.length; i++){ 
+                                                    //creo ciclo
+        myArray.push(newArray[i].charAt(0))
+        
 
+    }
+
+
+
+    return myArray
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let risultato = createArray(names)   // variabile con risultato della funzione
+console.log(risultato)
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
